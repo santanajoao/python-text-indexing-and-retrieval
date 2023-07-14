715ea61c2c356114a8e0a6ce2ecbdfc54949243d
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-07-14 19:57:04 -0300

    implementa função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str) -> list[str]:
+    try:
+        if not path_file.endswith(".txt"):
+            print("Formato inválido", file=sys.stderr)
+        else:
+            with open(path_file) as text_file:
+                content = text_file.read()
+            lines = content.split("\n")
+            return lines
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
