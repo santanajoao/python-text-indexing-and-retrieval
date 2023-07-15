d2f0002d7591f07af3a16b0a456a9f87211fad8d
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-07-14 23:41:01 -0300

    implementa a função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
+def find_word(word: str, lines: list[str]) -> list[int]:
+    occurrences = []
+    for index, line in enumerate(lines):
+        if word.lower() in line.lower():
+            occurrences.append({"linha": index + 1})
+    return occurrences
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result: list[dict[str]] = []
+    for file_data in instance:
+        ocurrences = find_word(word, file_data["linhas_do_arquivo"])
+        if len(ocurrences) > 0:
+            result.append({
+                "palavra": word,
+                "arquivo": file_data["nome_do_arquivo"],
+                "ocorrencias": ocurrences,
+            })
+    return result
 
 
 def search_by_word(word, instance):
