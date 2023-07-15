ba5a4aebf725c6d64faae71d1193c1cce1e2eccc
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-07-15 14:52:06 -0300

    refatora arquivo file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -3,49 +3,46 @@ from ting_file_management.file_management import txt_importer
 from ting_file_management.queue import Queue
 
 
-def get_name_from_process(process: dict[str]) -> str:
-    return process["nome_do_arquivo"]
+def get_name_from_file(file: dict[str]) -> str:
+    return file["nome_do_arquivo"]
 
 
 def get_file_lines(
-        path_file: str, instance: Queue) -> None | list[str]:
-    process = instance.find(path_file, key=get_name_from_process)
-    if process is not None:
-        return None
+        path_file: str, file_processes: Queue) -> None | list[str]:
+    file_data = file_processes.find(path_file, key=get_name_from_file)
+    if file_data is not None:
+        return []
 
     file_lines = txt_importer(path_file)
-    if len(file_lines) == 0:
-        return None
-
     return file_lines
 
 
-def process(path_file: str, instance: Queue):
-    file_lines = get_file_lines(path_file, instance)
-    if file_lines is None:
+def process(path_file: str, file_processes: Queue) -> None:
+    file_lines = get_file_lines(path_file, file_processes)
+    if len(file_lines) == 0:
         return None
 
-    process_data = {
+    file_data = {
         "nome_do_arquivo": path_file,
         "qtd_linhas": len(file_lines),
         "linhas_do_arquivo": file_lines,
     }
 
-    instance.enqueue(process_data)
-    print(process_data)
+    file_processes.enqueue(file_data)
+    print(file_data)
 
 
-def remove(instance: Queue):
+def remove(file_processes: Queue) -> None:
     try:
-        file_data: dict[str] = instance.dequeue()
+        file_data: dict[str] = file_processes.dequeue()
         print(f"Arquivo {file_data['nome_do_arquivo']} removido com sucesso")
     except IndexError:
         print("Não há elementos")
 
 
-def file_metadata(instance: Queue, position: int):
+def file_metadata(file_processes: Queue, position: int) -> None:
     try:
-        file_data: dict[str] = instance.search(position)
+        file_data: dict[str] = file_processes.search(position)
         print(file_data)
     except IndexError:
         print("Posição inválida", file=sys.stderr)
